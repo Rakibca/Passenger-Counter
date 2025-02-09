ede5d49906419d16d603a054db8a383dce3ab569
Rakibul Islam <rakib.ca@gmail.com>
2025-02-08 20:51:36 -0700

    added save button

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-// document.getElementById("count-el").innerText = 5
-
-// change the count-el in the HTML to reflect the new count
-
-// camelCase
 let countEl = document.getElementById("count-el");
-
-console.log(countEl);
-
 let count = 0;
 
 function increment() {
   count = count + 1;
   countEl.innerText = count;
+}
+
+function save() {
+  console.log(count);
 }
